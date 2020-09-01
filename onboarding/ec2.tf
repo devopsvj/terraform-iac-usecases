842bb6d0c144d1797362e05f89e5297232956ed8
ginigangadharan <net.gini@gmail.com>
2020-09-01 08:39:19 +0800

    ok

--- onboarding/ec2.tf
+++ onboarding/ec2.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region     = "ap-southeast-1"
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
+  version = ">=2.0"
 }
 
 resource "aws_instance" "web" {
