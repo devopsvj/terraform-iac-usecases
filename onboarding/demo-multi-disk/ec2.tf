10e704413c2599d810f943c2e99ad69a9b011d26
ginigangadharan <net.gini@gmail.com>
2020-09-13 10:17:21 +0800

    ok

--- onboarding/demo-multi-disk/ec2.tf
+++ onboarding/demo-multi-disk/ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region     = "ap-southeast-1"
+  region                  = "ap-southeast-1"
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
-  version = ">=2.0"
+  version                 = ">=2.0"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0cd31be676780afa7"
-  instance_type = "t2.micro"
+  ami               = "ami-0cd31be676780afa7"
+  instance_type     = "t2.micro"
   availability_zone = "ap-southeast-1a"
 
   tags = {
